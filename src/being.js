bb58ece9168a5f951c7df9ee48aed1ddb07bbe84
Suzie Q <suzieqwolf@gmail.com>
2020-03-22 22:00:19 -0600

    Commit being src and test

--- src/being.js
+++ src/being.js
@@ -1,4 +1,15 @@
 class Being {
+    constructor ( name, species, credits) {
+        this.isAlive = true;
+        this.name = name;
+        this.species = species;
+        this.credits = 0;
+    };
+
+    updateCredits(newCredits){
+        this.credits = this.credits + newCredits;
+        //return newCredits;
+    }
 }
 
 module.exports = Being;
