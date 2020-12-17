d74a729c0d448d92adcb7847f51142d1ad29222c
Suzie Q <suzieqwolf@gmail.com>
2020-12-16 17:27:52 -0700

    Adds changes to Shop class.

--- src/shop.js
+++ src/shop.js
@@ -1,21 +1,11 @@
 var Part = require('../src/part');
-var Being = require('../src/being');
-var Ship = require('../src/ship');
 
 class Shop {
-    constructor ({
-        name,
-        inventory = { },
-        item,
-        value,
-        broken,
-
-    })
-        {
-        this.name = name;
-        this.inventory = inventory;
-        this.item = item;
-        this.value = value;
+    constructor(shop) {
+        this.name = shop.name;
+        this.inventory = shop.inventory || {};
+        this.item = shop.item;
+        this.value = shop.value;
         this.broken = false;
         this.validParts = [
             "shell",
@@ -24,8 +14,8 @@ class Shop {
             "life support",
             "landing gear"
         ]
-    
-        }               
+    }
+
     addInventory(item) { 
         if(item instanceof Part) {
             this.inventory[item.type] = item;
@@ -33,29 +23,24 @@ class Shop {
     }
 
     outfitShip(ship, part) {
-        var hasCaptain = Ship.captain;
+        var hasCaptain = ship.captain;
         var needPart = this.inventory[part].type === part;
-        var captainCredits = hasCaptain ? Ship.captain.credits : 0;
+        var captainCredits = hasCaptain ? ship.captain.credits : 0;
         var needCredits = captainCredits < this.inventory[part].value;  
-
-        var diffCredits = (this.inventory[part].value) - captainCredits;
-        
-        
+        var diffCredits = (this.inventory[part].value) - captainCredits;        
         if (!hasCaptain) {
            return "cannot outfit a ship without a captian";
         } else if (needPart && needCredits) {
             return `you require ${diffCredits} more credits to make this purchase`;
-        }  
-          else { 
+        } else { 
             ship.parts[part] = this.inventory[part];
+            ship.captain.credits = captainCredits - this.inventory[part].value
             delete this.inventory[part];
             return `${part} added to ship`;
         }        
-        
-            //if (needPart && (captainCredits >= this.inventory[part].value))
-        }    
-
+    }
 
 }    
+
 
 module.exports = Shop;
