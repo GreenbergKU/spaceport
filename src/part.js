5950fc6e8c2ced186240c1f4dc284a9d045a26e0
Suzie Q <suzieqwolf@gmail.com>
2020-12-16 17:25:55 -0700

    Adds changes to Part class.

--- src/part.js
+++ src/part.js
@@ -1,24 +1,24 @@
-
 class Part {
     constructor(object) {
-        this.name = object.name;
-        //this.type = object.type;
-        this.validType = ['landing gear', 'shell', 'hyperdrive', 'computer', 'life support', undefined]
-        this.type = this.validType.includes(object.type) ? object.type : undefined;
-        this.value = object.value;
-        this.broken = false;
-            //* expression evaluates to a boolean with a value of true or false
-            //* defaults to true if undefined or null
-        //this.type = object.type;
-    }
-
+      this.name = object.name;
+      this.type = object.type;
+      this.validType = ['landing gear', 'shell', 'hyperdrive', 'computer', 'life support', undefined]
+      this.type = this.validType.includes(object.type) ? object.type : undefined;
+      this.value = object.value;
+      this.broken = false;
+    }        
+        //* expression evaluates to a boolean with a value of true or false
+        //* defaults to true if undefined or null
+      //this.type = object.type;
+    
     isValid() {
       if (this.name === undefined || this.type === undefined || this.value === undefined) {
         return false;
-        } //else {
+      }
       return true;
-      //}
     }
 
 }
+
+
 module.exports = Part;
